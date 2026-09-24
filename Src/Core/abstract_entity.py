90962f26d9c236a63116df72872dd3fe30b100e2
Alabaykin <bells.vtv@gmail.com>
2026-09-24 21:29:52 +0800

    Add id setter and equal method

--- Src/Core/abstract_entity.py
+++ Src/Core/abstract_entity.py
@@ -1,6 +1,6 @@
 from abc import ABC
 import uuid
-
+from Src.Core.exception import arguments_exception
 
 class abstract_entity(ABC):
     """
@@ -20,6 +20,15 @@ class abstract_entity(ABC):
             self._id = str(uuid.uuid4())
         return self._id
 
+    @id.setter
+    def id(self, value: str) -> None:
+        """
+        Установка id сущности.
+        """
+        if not value or not isinstance(value, str) or not value.strip():
+            raise arguments_exception(value, "Идентификатор должен быть непустой строкой", "id")
+        self._id = value.strip()
+
     @property
     def name(self) -> str:
         """
@@ -33,5 +42,14 @@ class abstract_entity(ABC):
         Установка наименования.
         """
         if not value or not isinstance(value, str) or not value.strip():
-            raise ValueError("Имя должно быть непустой строкой")
+            raise arguments_exception(value, "Имя должно быть непустой строкой", "name")
         self._name = value.strip()
+
+    def __eq__(self, other: object) -> bool:
+        """
+        Сравнение двух сущностей по идентификатору id.
+        """
+        if not isinstance(other, abstract_entity):
+            return False
+        return self.id == other.id
+
